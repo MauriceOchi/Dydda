2a32bfe81de03abb6e7ab6843f6de5119b2e42c6
MauriceOchi <mauriceochieng45mo@gmail.com>
2022-07-18 13:39:24 +0300

    workstation branch

---
+++ enum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    enum gender {male, female};
+    enum gender myGender = male;
+    enum gender anotherGender = female;
+
+    bool isMale = (myGender == anotherGender);
+    //find out how to print bool
+    printf("%")
+
+
+    return 0;
+}
