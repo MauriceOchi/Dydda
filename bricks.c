4545bde2d746555f3ca8a67bf0ea63745e24c01d
MauriceOchi <mauriceochieng45mo@gmail.com>
2022-07-27 19:08:05 +0300

    made bricks.c dynamic to take user input

--- bricks.c
+++ bricks.c
@@ -8,12 +8,12 @@
 int main(void)
 {
     int n;
-    puts("Enter number: \n");
+    puts("Enter number: ");
     scanf("%d", &n);
 
-    for(int i = 0; i <= n; i++)
+    for(int i = 0; i < n; i++)
     {
-        for(int j = i; j <= n; j++)
+        for(int j = i; j < n; j++)
         {
            printf("#");
         }
